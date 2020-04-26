9467f063e0e5234f24da630ec373d782e556b45b
André Barnabá Silva <asakeron@gmail.com>
2020-04-26 01:36:29 -0300

    Sway cofig
    Implement systemd --user eviroment cleanup to fix sway restart;
    start sway as a user service;
    use sway.target as a unit of coordination for desktop related services;
    start waybar as a user service after sway init.

--- sway/.config/fish/config.d/start-sway.fish
+++ sway/.config/fish/config.d/start-sway.fish
@@ -1,5 +1,6 @@
 if status --is-login; and test (tty) = "/dev/tty1"
-  systemctl  --wait --user import-environment XDG_SEAT XDG_SESSION_ID XDG_VTNR
+  systemctl --user unset-environment SWAYSOCK WAYLAND_DISPLAY
+  systemctl --user import-environment XDG_SEAT XDG_SESSION_ID XDG_VTNR
   exec systemctl --wait --user start sway
 end
 
